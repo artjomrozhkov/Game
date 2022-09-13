3beb4a902f4ce9d27ce0d78fa61e516a33f29ac5
opilane <opilane@HP-CZC6297BB7>
2022-09-13 11:46:23 +0300

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mäng
+{
+    public interface IEntity
+    {
+        string Info();
+        int pointsNumber();
+    }
+    class Item : IEntity
+    {
+        public string Name;
+        public int pointNumber;
+
+        public Item(string Name, int pointNumber)
+        {
+            this.Name = Name;
+            this.pointNumber = pointNumber;
+        }
+
+        public int pointsNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        string IEntity.Info()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
