445ce675fcc27e5d9154fa88bcd6b366efdb90f5
Artjom Rozhkov <93324182+artjomrozhkov@users.noreply.github.com>
2022-09-15 11:14:29 +0300

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace Mäng
 {
-    //    2. Klass Ese realiseerib liidese Üksus.
-    public class Ese : IEntity
+    //    2. Klass Item realiseerib liidese Üksus.
+    public class Item : IEntity
     {
         //1. Klassis on privaatsed isendiväljad järgmise info jaoks: nimetus(String) ja punktide arv(int).
         private string nimetus;
         private int PunktideArv;
 
         //2. Klassis peab olema kahe parameetriga konstruktor, mille abil saab määrata nimetuse ja punktide
-        public Ese(int punktideArv, string nimetus)
+        public Item(int punktideArv, string nimetus)
         {
             this.nimetus = nimetus;
             this.PunktideArv = punktideArv;
