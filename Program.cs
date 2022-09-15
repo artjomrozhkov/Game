99662e95afbe2c64b7c82a1085437546c00c8699
Artjom Rozhkov <93324182+artjomrozhkov@users.noreply.github.com>
2022-09-15 11:11:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+
+using Mäng;
+using System.Text;
+
+//Andmete fail  esemed.txt salvestage oma arvutisse. Fail peab olema kodeeringus UTF-8.
+Console.OutputEncoding = Encoding.UTF8;
+Peaklass.Game();
