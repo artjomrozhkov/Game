e01e9932dff0bbbe36d34f39d53c9c6eb5941c5e
Artjom Rozhkov <93324182+artjomrozhkov@users.noreply.github.com>
2022-09-20 10:01:48 +0300

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -6,27 +6,21 @@ using System.Threading.Tasks;
 
 namespace Mäng
 {
-    //    2. Klass Item realiseerib liidese Üksus.
     public class Item : IEntity
     {
-        //1. Klassis on privaatsed isendiväljad järgmise info jaoks: nimetus(String) ja punktide arv(int).
         private string nimetus;
         private int PunktideArv;
-
-        //2. Klassis peab olema kahe parameetriga konstruktor, mille abil saab määrata nimetuse ja punktide
         public Item(int punktideArv, string nimetus)
         {
             this.nimetus = nimetus;
             this.PunktideArv = punktideArv;
         }
 
-        //3. Meetod punktideArv tagastab punktide arvu.
         public int punktideArv()
         {
             return PunktideArv;
         }
 
-        //4. Meetod info tagastab selle eseme nimetuse.
         public string meetodInfo()
         {
             Console.WriteLine(nimetus);
